c191a74285ef3775fee4b35dc45d81a78ad750c5
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-11-03 10:29:44 +0500

    #fix #Data has been released from the API #UpdateUI

---
+++ src/js/UpdateUI.js
@@ -0,0 +1,27 @@
+const cardsEL = document.querySelector('.cards')
+
+export const createCountries = (countries) => {
+  countries.forEach((country) => {
+    const commonName = country.name.common;
+    const population = country.population
+    const region = country.region;
+    const capital = country.capital ? country.capital[0] : "No Capital";
+    const flag = country.flags.svg;
+
+    const li = document.createElement("li");
+    li.classList.add("cars__item");
+    li.innerHTML = `
+    <a href="./about.html">
+      <img src=${flag} alt="germany-flag" width="267" height="160">
+        <div class="cards__item-inner">
+          <h3 class="cards__title">${commonName}</h3>
+          <p class="population">Population: <span>${population}</span></p>
+          <p class="region">Region: <span>${region}</span></p>
+          <p class="capital">Capital: <span>${capital}</span></p>
+        </div>
+    </a>
+    `;
+
+    cardsEL.appendChild(li)
+  });
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,12 @@
 import "../css/index.css"
 
 import request from "./request"
+import { createCountries } from "./UpdateUI"
 
 const API = "https://restcountries.com/v3.1/all"
 
 request(API).then((data) =>{
-    console.log(data);
+    createCountries(data);
 }).catch((error) =>{
     console.error(error.message);
 })
