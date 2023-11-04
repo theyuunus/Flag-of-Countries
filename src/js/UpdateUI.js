cc2de4e20b9c4b47d6d85348dbd84b8b92c58a5c
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-11-04 11:50:26 +0500

    #add About

--- src/js/UpdateUI.js
+++ src/js/UpdateUI.js
@@ -1,10 +1,10 @@
-const cardsEL = document.querySelector('.cards')
+const cardsEL = document.querySelector(".cards");
 
 export const createCountries = (countries) => {
-  cardsEL.innerHTML =''
+  cardsEL.innerHTML = "";
   countries.forEach((country) => {
     const commonName = country.name.common;
-    const population = country.population
+    const population = country.population;
     const region = country.region;
     const capital = country.capital ? country.capital[0] : "No Capital";
     const flag = country.flags.svg;
@@ -12,7 +12,7 @@ export const createCountries = (countries) => {
     const li = document.createElement("li");
     li.classList.add("cards__item");
     li.innerHTML = `
-    <a href="./about.html">
+    <a href="./about.html?country=/name/${commonName}">
       <img src=${flag} alt="germany-flag" width="267" height="160">
         <div class="cards__item-inner">
           <h3 class="cards__title">${commonName}</h3>
@@ -22,6 +22,82 @@ export const createCountries = (countries) => {
         </div>
     </a>
     `;
-    cardsEL.appendChild(li)
+    cardsEL.appendChild(li);
   });
+};
+
+const countryInfoEl = document.querySelector(".country-info");
+
+export const createCountryInfo = (country) => {
+  const {
+    population,
+    borders,
+    capital,
+    flags,
+    name,
+    region,
+    tld,
+    currencies,
+    languages,
+    subregion,
+  } = country;
+
+  const nativeName = Object.values(name.nativeName)[0].official;
+  const currency = Object.values(currencies)[0].name;
+  const language = Object.values(languages);
+
+  countryInfoEl.innerHTML = `
+  <img class="country-info__img" src=${flags.svg} alt="germany-flag" width="560"
+  height="400" />
+<div class="country-info__content">
+  <h2>${name.common}</h2>
+  <ul class="country-info__list">
+      <li class="country-info__item">
+          <p class="name">
+              Native Name:
+              <span>${nativeName}</span>
+          </p>
+          <p class="population">
+              Population:
+              <span>${population}</span>
+          </p>
+          <p class="region">
+              Region:
+              <span>${region}</span>
+          </p>
+          <p class="sub-region">
+              Sub Region:
+              <span>${subregion}</span>
+          </p>
+          <p class="capital">
+              Capital:
+              <span>${capital}</span>
+          </p>
+      </li>
+      <li class="country-info__item">
+          <p class="name">
+              Top Level Domain:
+              <span>${tld}</span>
+          </p>
+          <p class="population">
+              Currencies:
+              <span>${currency}</span>
+          </p>
+          <p class="region">
+              Languages:
+              <span>${language}</span>
+          </p>
+      </li>
+  </ul>
+
+   <div class="country-info__borders">
+      <h3>Border Countries:</h3>
+      ${borders ? borders.map((border) => {
+        return `          
+        <a href="./about.html?country=/alpha/${border}">${border}</a>
+        `
+      }) : "No Borders"}
+   </div>
+ </div>
+  `;
 };
--- src/js/about.js
+++ src/js/about.js
@@ -1,1 +1,16 @@
-import "../css/index.css"
+import "../css/index.css";
+import "./mode";
+
+import request from "./request";
+import { createCountryInfo } from "./UpdateUI";
+
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString)
+const country = urlParams.get('country')
+const countryAPI = `https://restcountries.com/v3.1/${country}`
+
+request(countryAPI).then((data) => {
+    createCountryInfo(data[0])
+}).catch((error) => {
+    console.error(error.message);
+})
